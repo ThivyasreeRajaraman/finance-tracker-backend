7bf667a5f40ad4fab998673693d1e99437e2ff95
Thivyasree-Rajaraman <thivyasree.ktr@shopup.org>
2024-05-23 17:33:05 +0530

    feat(user): add user update functionality and test cases

---
+++ controllers/user/controller_suite_test.go
@@ -0,0 +1,25 @@
+package controllers
+
+import (
+	"os"
+	"testing"
+
+	"github.com/Thivyasree-Rajaraman/finance-tracker/initializers"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+func init() {
+	initializers.LoadEnvVariables(true)
+	initializers.InitDB(os.Getenv("TEST_DATABASE_URL"))
+	initializers.SyncDatabase()
+}
+
+func TestControllerService(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Controller Suite")
+}
+
+var _ = BeforeSuite(func() {
+	initializers.DB.Exec("TRUNCATE TABLE users CASCADE")
+})
---
+++ controllers/user/user_controller.go
@@ -0,0 +1,30 @@
+package controllers
+
+import (
+	"net/http"
+
+	"github.com/Thivyasree-Rajaraman/finance-tracker/models"
+	userservices "github.com/Thivyasree-Rajaraman/finance-tracker/services/user"
+	"github.com/Thivyasree-Rajaraman/finance-tracker/utils"
+	"github.com/gin-gonic/gin"
+)
+
+func Update(c *gin.Context) {
+	userInterface, _ := c.Get("currentUser")
+	user, err := userInterface.(models.User)
+	if !err {
+		utils.HandleError(c, http.StatusBadRequest, "Invalid user data", nil)
+	}
+
+	if err := utils.UnmarshalData(c, &user); err != nil {
+		utils.HandleError(c, http.StatusBadRequest, "Failed to unmarshal request body", err)
+		return
+	}
+
+	if err := userservices.UpdateUser(c, &user); err != nil {
+		utils.HandleError(c, http.StatusInternalServerError, "Failed to update user", err)
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"success": true, "message": "User updated successfully", "user": user})
+}
